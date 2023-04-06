7952891e370fe37c08d69b5079ab8170c2624642
Dileep Bokka <120390497+BK-Dileep@users.noreply.github.com>
2023-04-06 19:31:52 +0530

    variables.tf

--- variables.tf
+++ variables.tf
@@ -1,9 +1,3 @@
-variable "access_key" {
-  default = "AKIAXTZMANOFIU5FNDFA"
-  }
-variable "secret_key" {
-  default = "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"
-  }
 variable "region" {
   default = "us-east-1"
   }
