360983c980efb24a77bb33ac8534e601b75214cb
Dileep Bokka <120390497+BK-Dileep@users.noreply.github.com>
2023-04-06 18:58:34 +0530

    jenkins.tf

---
+++ jenkins.tf
@@ -0,0 +1,39 @@
+pipeline {
+    agent any
+    
+    tools {
+        terraform 'terraform'
+    }
+    stages {
+        stage ("checkout from GIT") {
+            steps {
+                git branch: 'main', credentialsId: 'BK-Dileep', url: 'https://github.com/BK-Dileep/terraform.git'
+            }
+        }
+        stage ("terraform init") {
+            steps {
+                sh 'terraform init'
+            }
+        }
+        stage ("terraform fmt") {
+            steps {
+                sh 'terraform fmt'
+            }
+        }
+        stage ("terraform validate") {
+            steps {
+                sh 'terraform validate'
+            }
+        }
+        stage ("terrafrom plan") {
+            steps {
+                sh 'terraform plan '
+            }
+        }
+        stage ("terraform apply") {
+            steps {
+                sh 'terraform apply --auto-approve'
+            }
+        }
+    }
+}
